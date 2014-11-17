14f4286bee2cb7be44a99310d8f8484ff4ba842f
yokoi.shinya <shinya.yokoi@gmail.com>
2014-11-17 12:32:56 +0900

    20141117

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,6 +2,7 @@ require 'spec_helper'
 
 describe "Static pages" do
 
+
   describe "Home page" do
 
     it "should have the content 'Sample App'" do
@@ -16,6 +17,15 @@ describe "Static pages" do
     it "should have the content 'Help'" do
       visit '/static_pages/help'
       expect(page).to have_content('Help')
+    end
+  end
+
+
+  describe "About page" do
+
+    it "should have the content 'About Us'" do 
+      visit '/static_pages/about'
+      expect(page).to have_content('About Us')
     end
   end
 
